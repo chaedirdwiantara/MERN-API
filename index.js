05a311e4c5f5d4567d09c5d9f0d1d7af18f8794c
chaedirdwiantara <chaedirdwiantara@gmail.com>
2021-10-25 22:33:29 +0700

    send data from Body with method POST

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express"); //import express
+const bodyParser = require("body-parser");
 
 const app = express();
 const productRoutes = require("./src/routes/products");
+
+app.use(bodyParser.json()); // type JSON
+
+//Allow error CORS origin
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/v1/customer", productRoutes);
 
